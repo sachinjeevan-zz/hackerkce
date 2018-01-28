715a63bad35c7cf4416e35e921145e8086a7fe45
sachinjeevan <sachinjeevan2506@gmail.com>
2018-01-28 02:51:03 -0800

    db

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,20 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'http://hackerkce.herokuapp.com' }
+
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.perform_deliveries = true
+
+  ActionMailer::Base.smtp_settings = {
+      :address              => 'smtp.gmail.com',
+      :port                 => '587',
+      :domain               => 'hackerkce.herokuapp.com',
+      :user_name            => 'sachinjeevan2506@gmail.com',
+      :password             => 'f_selvapriya',
+      :authentication       => "plain",
+      :enable_starttls_auto => true
+  }
+
 end
